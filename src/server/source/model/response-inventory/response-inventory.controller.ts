4e1b5ebc607d4b5f2901dd1b33750d0b5e90a242
NotHydra <rizky.irswanda115@gmail.com>
2024-06-11 07:02:08 +0800

    feat(server): find latest controller

--- src/server/source/model/response-inventory/response-inventory.controller.ts
+++ src/server/source/model/response-inventory/response-inventory.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, ForbiddenException, Param, ParseIntPipe, UseInterceptors } from "@nestjs/common";
+import { Body, Controller, ForbiddenException, Get, Param, ParseIntPipe, UseInterceptors } from "@nestjs/common";
 import { ResponseInventory } from "@prisma/client";
 
 import { Override } from "../../common/decorator/override";
-import { ResponseFormatInterceptor } from "../../common/interceptor/response-format.interceptor";
+import { formatResponse, ResponseFormatInterceptor } from "../../common/interceptor/response-format.interceptor";
 import { ResponseFormatInterface } from "../../common/interface/response-format";
 
 import { BaseController } from "../../global/base.controller";
@@ -25,6 +25,22 @@ export class ResponseInventoryController
 {
     constructor(modelService: ResponseInventoryService) {
         super(ResponseInventoryController.name, modelService);
+    }
+
+    @Get("latest")
+    public async findLatest(): Promise<ResponseFormatInterface<ResponseInventoryModel[]>> {
+        try {
+            const response: ResponseFormatInterface<ResponseInventoryModel[]> = formatResponse<
+                ResponseInventoryModel[]
+            >(true, 200, "Found Latest", await this.modelService.findLatest());
+
+            this.loggerService.log(`Find Latest: ${JSON.stringify(response)}`);
+
+            return response;
+        } catch (error) {
+            this.loggerService.error(`Find Latest: ${error.message}`);
+            return formatResponse<null>(false, 500, error.message, null);
+        }
     }
 
     @Override
