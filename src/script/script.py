dd9893edccffddf39d522fa7f144e64eaa72e385
NotHydra <rizky.irswanda115@gmail.com>
2024-06-13 18:28:03 +0800

    feat(script): iterate through all frequencies 10 times

--- src/script/script.py
+++ src/script/script.py
@@ -38,6 +38,8 @@ def log(message: str) -> None:
 
 URL: str = os.getenv("URL")
 
+FREQUENCY: list[float] = [919.5, 920.0, 920.5, 921.0, 921.5, 922.0, 922.5]
+
 GPIO.setmode(GPIO.BCM)
 
 BUTTON_PIN: int = 18
@@ -48,150 +50,125 @@ GPIO.setup(BUTTON_PIN, GPIO.IN)
 GPIO.setup(LED_GREEN_PIN, GPIO.OUT)
 GPIO.setup(LED_RED_PIN, GPIO.OUT)
 
-log("Starting RFID reader...")
+log("Initializing RFID Reader...")
 log(f"Ports: {SerialTransport.scan()}")
 
-port: str = SerialTransport.scan()[0]
-log(f"Port: {port}")
+PORT: str = SerialTransport.scan()[0]
+log(f"Port: {PORT}")
 
-transport: SerialTransport = SerialTransport(
-    serial_port=port, baud_rate=BaudRate.BPS_115200, timeout=1
+TRANSPORT: SerialTransport = SerialTransport(
+    serial_port=PORT, baud_rate=BaudRate.BPS_115200, timeout=1
 )
-log(f"Transport: {transport}")
+log(f"Transport: {TRANSPORT}")
 
-reader: Reader = Reader(transport)
-log("RFID reader started")
-
-wiegand: Wiegand = Wiegand(
-    is_open=False,
-    byte_first_type=WiegandByteFirstType.LOW_BYTE_FIRST,
-    protocol=WiegandProtocol.WG_26,
-)
-
-antenna: Antenna = Antenna(
-    ant_1=True,
-    ant_2=False,
-    ant_3=False,
-    ant_4=False,
-    ant_5=False,
-    ant_6=False,
-    ant_7=False,
-    ant_8=False,
-)
-
-frequency: Frequency = Frequency(
-    region=REGION_MALAYSIA,
-    min_frequency=920.00,
-    max_frequency=925.00,
-)
-
-reader.set_reader_settings(
-    ReaderSettings(
-        address=0,
-        rfid_protocol=RfidProtocol.ISO_18000_6C,
-        work_mode=WorkMode.ACTIVE_MODE,
-        output_interface=OutputInterface.USB,
-        baud_rate=BaudRate.BPS_115200,
-        wiegand=wiegand,
-        antenna=antenna,
-        frequency=frequency,
-        power=30,  # max RC4 dbm
-        output_memory_bank=MemoryBank.EPC,
-        q_value=4,
-        session=Session.SESSION_0,
-        output_start_address=0,
-        output_length=12,
-        filter_time=0,
-        trigger_time=3,
-        buzzer_time=True,
-        inventory_interval=100,
-    )
-)
-
-answer_mode_inventory_parameters: AnswerModeInventoryParameter = (
-    AnswerModeInventoryParameter(
-        stop_after=StopAfter.NUMBER,
-        value=10,
-    )
-)
-
-response: Iterator[ResponseInventory] | None = reader.start_inventory(
-    work_mode=WorkMode.ANSWER_MODE,
-    answer_mode_inventory_parameter=answer_mode_inventory_parameters,
-)
-
-index: int = 1
-switchValue = False
+READER: Reader = Reader(TRANSPORT)
+log("RFID Reader Initialized...")
 
 try:
     while True:
         if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-            switchValue = not switchValue
+            log("Starting...")
 
-            log(f"Button: Pressed, {'Start' if switchValue else 'Pause'}")
-
-            time.sleep(1)
-
-        if not switchValue:
-            GPIO.output(LED_GREEN_PIN, GPIO.LOW)
-            GPIO.output(LED_RED_PIN, GPIO.HIGH)
-
-        else:
             GPIO.output(LED_GREEN_PIN, GPIO.HIGH)
             GPIO.output(LED_RED_PIN, GPIO.LOW)
 
-            for res in response:
-                log(f"({index}).InventoryThread() > run() > res: {res}")
+            for frequencyIndex, frequencyValue in enumerate(FREQUENCY, start=1):
+                log(f"Frequency {frequencyIndex}: {frequencyValue}MHz")
 
-                if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
-                    switchValue = not switchValue
+                READER.set_reader_settings(
+                    ReaderSettings(
+                        address=0,
+                        rfid_protocol=RfidProtocol.ISO_18000_6C,
+                        work_mode=WorkMode.ACTIVE_MODE,
+                        output_interface=OutputInterface.USB,
+                        baud_rate=BaudRate.BPS_115200,
+                        wiegand=Wiegand(
+                            is_open=False,
+                            byte_first_type=WiegandByteFirstType.LOW_BYTE_FIRST,
+                            protocol=WiegandProtocol.WG_26,
+                        ),
+                        antenna=Antenna(
+                            ant_1=True,
+                            ant_2=False,
+                            ant_3=False,
+                            ant_4=False,
+                            ant_5=False,
+                            ant_6=False,
+                            ant_7=False,
+                            ant_8=False,
+                        ),
+                        frequency=Frequency(
+                            region=REGION_MALAYSIA,
+                            min_frequency=frequencyValue,
+                            max_frequency=frequencyValue,
+                        ),
+                        power=30,
+                        output_memory_bank=MemoryBank.EPC,
+                        q_value=4,
+                        session=Session.SESSION_0,
+                        output_start_address=0,
+                        output_length=12,
+                        filter_time=0,
+                        trigger_time=3,
+                        buzzer_time=True,
+                        inventory_interval=100,
+                    )
+                )
 
-                    log(f"Button: Pressed, {'Start' if switchValue else 'Pause'}")
+                response: Iterator[ResponseInventory] | None = READER.start_inventory(
+                    work_mode=WorkMode.ANSWER_MODE,
+                    answer_mode_inventory_parameter=(
+                        AnswerModeInventoryParameter(
+                            stop_after=StopAfter.NUMBER,
+                            value=10,
+                        )
+                    ),
+                )
 
-                    time.sleep(1)
+                count: int = 1
+                for res in response:
+                    print()
 
-                if not switchValue:
-                    break
+                    # log(f"({count}).InventoryThread() > run() > res: {res}")
 
-                else:
                     if res is None:
                         continue
 
                     if res.status == InventoryStatus.SUCCESS and res.tag:
-                        log(res)
+                        # log(res)
+                        # log(
+                        #     f"Tag: {res.tag} - RSSI: {str(calculate_rssi(res.tag.rssi))[0:3]}"
+                        # )
                         log(
-                            f"Tag: {res.tag} - RSSI: {str(calculate_rssi(res.tag.rssi))[0:3]}"
+                            f"Frequency {frequencyIndex} RSSI {count}: {int(str(calculate_rssi(res.tag.rssi))[0:3])}dBm"
                         )
 
-                        try:
-                            response = requests.post(
-                                f"{URL}/api/response-inventory",
-                                json={
-                                    "rssi": str(res.tag.rssi),
-                                    "data": str(res.tag.data),
-                                    "rssiValue": int(
-                                        str(calculate_rssi(res.tag.rssi))[0:3]
-                                    ),
-                                },
-                            )
-
-                            log(f"Response: {response.json()}")
-                        except Exception as e:
-                            log(f"Response: {e}")
+                        count += 1
+                        if count > 10:
+                            break
 
                     if (
                         res.status == InventoryStatus.NO_COUNT_LABEL
-                        and reader.work_mode == WorkMode.ANSWER_MODE
+                        and READER.work_mode == WorkMode.ANSWER_MODE
                     ):
                         break
 
-                    index += 1
-                    time.sleep(1)
+                    time.sleep(0.25)
+
+                READER.stop_inventory()
+                print()
+
+            log("Finished...")
+
+        GPIO.output(LED_GREEN_PIN, GPIO.LOW)
+        GPIO.output(LED_RED_PIN, GPIO.HIGH)
+
+        time.sleep(1)
+
 
 except KeyboardInterrupt:
     pass
 
 finally:
     GPIO.cleanup()
-    reader.stop_inventory()
-    reader.close()
+    READER.close()
