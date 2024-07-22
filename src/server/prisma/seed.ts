5c280a441e870db0d3e58234f9855cdf30610ac6
NotHydra <rizky.irswanda115@gmail.com>
2024-07-22 14:21:34 +0800

    feat(server): default seeder

--- src/server/prisma/seed.ts
+++ src/server/prisma/seed.ts
@@ -1,7 +1,38 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, ActiveReaderConfiguration, ReaderConfiguration, FrequencyConfiguration } from "@prisma/client";
 
-const prisma = new PrismaClient();
-async function main(): Promise<void> {}
+const prisma: PrismaClient = new PrismaClient();
+async function main(): Promise<void> {
+    const readerConfiguration: ReaderConfiguration & { frequencyConfiguration: FrequencyConfiguration[] } =
+        await prisma.readerConfiguration.create({
+            data: {
+                name: "Default",
+                rssiScanCount: 10,
+                rssiScanInterval: 100,
+                frequencyConfiguration: {
+                    create: [
+                        { frequency: "919.5" },
+                        { frequency: "920" },
+                        { frequency: "920.5" },
+                        { frequency: "921" },
+                        { frequency: "921.5" },
+                        { frequency: "922" },
+                        { frequency: "922.5" },
+                    ],
+                },
+            },
+            include: {
+                frequencyConfiguration: true,
+            },
+        });
+
+    const activeReaderConfiguration: ActiveReaderConfiguration = await prisma.activeReaderConfiguration.create({
+        data: {
+            readerConfigurationId: readerConfiguration.id,
+        },
+    });
+
+    console.log(JSON.stringify({ readerConfiguration, activeReaderConfiguration }, null, 4));
+}
 
 main()
     .then(async (): Promise<void> => {
