f42c543ba72cf041f28f63a118a6eb3a45e0b989
NotHydra <rizky.irswanda115@gmail.com>
2024-07-25 01:01:44 +0800

    feat(website): reader configuration add implementation

--- src/website/src/app/dashboard/reader-configuration/add/page.tsx
+++ src/website/src/app/dashboard/reader-configuration/add/page.tsx
@@ -1,63 +1,147 @@
 "use client";
 
+import axios, { AxiosResponse } from "axios";
 import Link from "next/link";
+import { ChangeEvent, FormEvent, useState } from "react";
+import Swal, { SweetAlertResult } from "sweetalert2";
+
+import { ResponseFormatInterface } from "@/common/interface/response-format.interface";
+import { ReaderConfigurationModel } from "@/common/interface/reader-configuration.interface";
 
 export default function ReaderConfigurationAddPage(): JSX.Element {
+    const [name, setName] = useState<string>("");
+    const [rssiScanCount, setRssiScanCount] = useState<number>(0);
+    const [rssiScanInterval, setRssiScanInterval] = useState<number>(0);
+
+    const handleAdd = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault();
+
+        Swal.fire<void>({
+            icon: "warning",
+            title: "Are you sure?",
+            confirmButtonText: "Yes",
+            confirmButtonColor: "#3ABB81",
+            showCancelButton: true,
+            cancelButtonColor: "#FF6685",
+        }).then(async (result: SweetAlertResult<void>): Promise<void> => {
+            if (result.isConfirmed) {
+                try {
+                    await axios
+                        .post<
+                            ResponseFormatInterface<ReaderConfigurationModel>
+                        >("http://localhost:3001/api/reader-configuration", { name, rssiScanCount, rssiScanInterval })
+                        .then(
+                            (): void => {
+                                Swal.fire({
+                                    icon: "success",
+                                    title: "Success",
+                                    confirmButtonText: "Close",
+                                    confirmButtonColor: "#FF6685",
+                                });
+                            },
+                            (response: AxiosResponse<ResponseFormatInterface<ReaderConfigurationModel>>): void => {
+                                Swal.fire({
+                                    icon: "error",
+                                    title: "Error",
+                                    text: response.data.message,
+                                    confirmButtonText: "Close",
+                                    confirmButtonColor: "#FF6685",
+                                });
+                            }
+                        );
+                } catch (error) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Error",
+                        text: "Internal Server Error",
+                        confirmButtonText: "Close",
+                        confirmButtonColor: "#FF6685",
+                    });
+                }
+            }
+        });
+    };
+
     return (
         <div className="card has-background-white">
             <div className="card-content">
                 <div className="content">
-                    <div className="field" title="The name of the reader configuration">
-                        <label className="label" htmlFor="name">
-                            Name
-                        </label>
+                    <form onSubmit={handleAdd}>
+                        <div className="field" title="The name of the reader configuration">
+                            <label className="label" htmlFor="name">
+                                Name
+                            </label>
 
-                        <div className="control">
-                            <input className="input" type="text" name="name" placeholder="Insert name here" />
+                            <div className="control">
+                                <input
+                                    className="input"
+                                    type="text"
+                                    name="name"
+                                    value={name}
+                                    placeholder="Insert name here"
+                                    onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+                                />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="field" title="The amount of RSSI scan for each frequency">
-                        <label className="label" htmlFor="rssiScanCount">
-                            RSSI Scan Count
-                        </label>
+                        <div className="field" title="The amount of RSSI scan for each frequency">
+                            <label className="label" htmlFor="rssiScanCount">
+                                RSSI Scan Count
+                            </label>
 
-                        <div className="control">
-                            <input className="input" type="text" name="rssiScanCount" placeholder="Insert RSSI scan count here" />
+                            <div className="control">
+                                <input
+                                    className="input"
+                                    type="number"
+                                    name="rssiScanCount"
+                                    value={rssiScanCount === 0 ? "" : rssiScanCount}
+                                    min="0"
+                                    placeholder="Insert RSSI scan count here"
+                                    onChange={(e: ChangeEvent<HTMLInputElement>) => setRssiScanCount(Number(e.target.value))}
+                                />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="field" title="The amount of delay after each RSSI scan">
-                        <label className="label" htmlFor="rssiScanInterval">
-                            RSSI Scan Interval
-                        </label>
+                        <div className="field" title="The amount of delay after each RSSI scan">
+                            <label className="label" htmlFor="rssiScanInterval">
+                                RSSI Scan Interval {"(ms)"}
+                            </label>
 
-                        <div className="control">
-                            <input className="input" type="text" name="rssiScanInterval" placeholder="Insert RSSI scan interval here" />
+                            <div className="control">
+                                <input
+                                    className="input"
+                                    type="number"
+                                    name="rssiScanInterval"
+                                    value={rssiScanInterval == 0 ? "" : rssiScanInterval}
+                                    min="0"
+                                    placeholder="Insert RSSI scan interval here"
+                                    onChange={(e: ChangeEvent<HTMLInputElement>) => setRssiScanInterval(Number(e.target.value))}
+                                />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="buttons">
-                        <button className="button is-fullwidth is-success has-text-weight-bold" title="Add Action">
-                            <span className="icon">
-                                <i className="fas fa-plus"></i>
-                            </span>
+                        <div className="buttons">
+                            <button className="button is-fullwidth is-success has-text-weight-bold" type="submit" title="Add Action">
+                                <span className="icon">
+                                    <i className="fas fa-plus"></i>
+                                </span>
 
-                            <span>Add</span>
-                        </button>
+                                <span>Add</span>
+                            </button>
 
-                        <Link
-                            href="/dashboard/reader-configuration"
-                            className="button is-fullwidth is-danger has-text-white has-text-weight-bold"
-                            title="Back Action"
-                        >
-                            <span className="icon">
-                                <i className="fas fa-reply"></i>
-                            </span>
+                            <Link
+                                href="/dashboard/reader-configuration"
+                                className="button is-fullwidth is-danger has-text-white has-text-weight-bold"
+                                title="Back Action"
+                            >
+                                <span className="icon">
+                                    <i className="fas fa-reply"></i>
+                                </span>
 
-                            <span>Back</span>
-                        </Link>
-                    </div>
+                                <span>Back</span>
+                            </Link>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
