e3562d04aeae52197b1d7c320c0f0633f4952bc0
NotHydra <rizky.irswanda115@gmail.com>
2024-07-24 12:42:51 +0800

    feat(website): navigation bar

--- src/website/src/app/dashboard/layout.tsx
+++ src/website/src/app/dashboard/layout.tsx
@@ -5,6 +5,7 @@ import "bulma/css/bulma.css";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React from "react";
 
 import "./../../public/css/main.css";
 
@@ -14,13 +15,27 @@ export default function Layout({
     children: React.ReactNode;
 }>): JSX.Element {
     const pathName: string = usePathname();
+    const navigationPath: { [key: string]: { display: string; link: string }[] } = {
+        "/dashboard/home": [
+            { display: "Dashboard", link: "/dashboard/home" },
+            { display: "Home", link: "/dashboard/home" },
+        ],
+        "/dashboard/tag": [
+            { display: "Dashboard", link: "/dashboard/home" },
+            { display: "Tag", link: "/dashboard/tag" },
+        ],
+        "/dashboard/reader-configuration": [
+            { display: "Dashboard", link: "/dashboard/home" },
+            { display: "Reader Configuration", link: "/dashboard/reader-configuration" },
+        ],
+    };
 
     return (
         <div className="columns dashboard">
-            <aside className="column is-2 sidebar has-text-dark has-background-white pb-0 pr-0">
+            <aside className="column is-2 sidebar has-text-dark has-background-white line-right pb-0 pr-0">
                 <div className="line-bottom">
                     <Link href="/dashboard/home" className="navbar-item p-0">
-                        <h2 className="title is-3 is-fullwidth has-text-dark has-text-main-hover has-text-centered py-5">PARI</h2>
+                        <h2 className="title is-3 brand is-fullwidth has-text-dark has-text-main-hover has-text-centered py-4">PARI</h2>
                     </Link>
                 </div>
 
@@ -52,7 +67,25 @@ export default function Layout({
                 </ul>
             </aside>
 
-            <div className="column">{children}</div>
+            <div className="column pl-0">
+                <nav className="navbar is-white line-bottom py-2" role="navigation" aria-label="main navigation">
+                    <div className="navbar-menu">
+                        <div className="navbar-start pl-2">
+                            {navigationPath[pathName].map((item, index) => (
+                                <React.Fragment key={index}>
+                                    <Link href={item.link} className="navbar-item has-text-main-hover has-text-weight-medium">
+                                        {item.display}
+                                    </Link>
+
+                                    {index < navigationPath[pathName].length - 1 ? (
+                                        <span className="navbar-item has-text-weight-medium px-0"> {">"} </span>
+                                    ) : null}
+                                </React.Fragment>
+                            ))}
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </div>
     );
 }
