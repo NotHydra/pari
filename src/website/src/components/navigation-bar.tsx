a10f1addf3aa6959f2c25c4699e4f07c0bdac312
NotHydra <rizky.irswanda115@gmail.com>
2024-07-25 11:53:13 +0800

    feat(website): dynamic navigation bar

--- src/website/src/components/navigation-bar.tsx
+++ src/website/src/components/navigation-bar.tsx
@@ -1,35 +1,41 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import React from "react";
 
 export default function NavigationBar(): JSX.Element {
     const pathName: string = usePathname();
-    const navigationPath: { [key: string]: { display: string; link: string }[] } = {
-        "/dashboard": [
-            { display: "Dashboard", link: "/dashboard" },
-            { display: "Home", link: "/dashboard" },
-        ],
-        "/dashboard/tag": [
-            { display: "Dashboard", link: "/dashboard" },
-            { display: "Tag", link: "/dashboard/tag" },
-        ],
-        "/dashboard/reader-configuration": [
-            { display: "Dashboard", link: "/dashboard" },
-            { display: "Reader Configuration", link: "/dashboard/reader-configuration" },
-        ],
-        "/dashboard/reader-configuration/add": [
-            { display: "Dashboard", link: "/dashboard" },
-            { display: "Reader Configuration", link: "/dashboard/reader-configuration" },
-            { display: "Add", link: "/dashboard/reader-configuration/add" },
-        ],
-        "/dashboard/reader-configuration/1/change": [
-            { display: "Dashboard", link: "/dashboard" },
-            { display: "Reader Configuration", link: "/dashboard/reader-configuration" },
-            { display: "Change", link: "/dashboard/reader-configuration/1/change" },
-        ],
-    };
+    const params: { readerConfigurationId: string } = useParams<{ readerConfigurationId: string }>();
+
+    const navigationPath: { [key: string]: { display: string; link: string }[] } = {};
+
+    navigationPath["/dashboard"] = [
+        { display: "Dashboard", link: "/dashboard" },
+        { display: "Home", link: "/dashboard" },
+    ];
+
+    navigationPath["/dashboard/tag"] = [
+        { display: "Dashboard", link: "/dashboard" },
+        { display: "Tag", link: "/dashboard/tag" },
+    ];
+
+    navigationPath["/dashboard/reader-configuration"] = [
+        { display: "Dashboard", link: "/dashboard" },
+        { display: "Reader Configuration", link: "/dashboard/reader-configuration" },
+    ];
+
+    navigationPath["/dashboard/reader-configuration/add"] = [
+        { display: "Dashboard", link: "/dashboard" },
+        { display: "Reader Configuration", link: "/dashboard/reader-configuration" },
+        { display: "Add", link: "/dashboard/reader-configuration/add" },
+    ];
+
+    navigationPath[`/dashboard/reader-configuration/${params.readerConfigurationId}/change`] = [
+        { display: "Dashboard", link: "/dashboard" },
+        { display: "Reader Configuration", link: "/dashboard/reader-configuration" },
+        { display: "Change", link: `/dashboard/reader-configuration/${params.readerConfigurationId}/change` },
+    ];
 
     return (
         <div className="column pl-0 pb-0">
