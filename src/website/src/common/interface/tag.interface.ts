1d069dbba776e07a1ab41c49c19fe44e39b1bdbc
NotHydra <rizky.irswanda115@gmail.com>
2024-08-06 00:18:43 +0800

    feat(website): tag detailed model

--- src/website/src/common/interface/tag.interface.ts
+++ src/website/src/common/interface/tag.interface.ts
@@ -1,8 +1,14 @@
+import { FrequencyDetailedModel } from "./frequency.interface";
+
 export interface TagModel {
     id: number;
     readerConfigurationId: number;
     tag?: string;
     createdAt: Date;
+}
+
+export interface TagDetailedModel extends TagModel {
+    frequency: FrequencyDetailedModel[];
 }
 
 export interface TagTableModel extends TagModel {
