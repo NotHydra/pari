ce708713767eb9e4980d6d76d92538b009445180
NotHydra <rizky.irswanda115@gmail.com>
2024-08-06 00:18:20 +0800

    feat(website): frequency detailed model

--- src/website/src/common/interface/frequency.interface.ts
+++ src/website/src/common/interface/frequency.interface.ts
@@ -1,7 +1,13 @@
+import { RSSIModel } from "./rssi.interface";
+
 export interface FrequencyModel {
     id: number;
     tagId: number;
     frequency: string;
+}
+
+export interface FrequencyDetailedModel extends FrequencyModel {
+    rssi: RSSIModel[];
 }
 
 export interface FrequencyTableModel extends FrequencyModel {
